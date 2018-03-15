90361fcba5905c51ba69352399dfc1893bbe8cca
jiaojiaohanhan <870303709@qq.com>
2018-03-15 23:04:04 +0800

    newphp

--- Ques9.php
+++ Ques9.php
@@ -1,2 +1,77 @@
 <?php
+//for($i=1;$i<10;$i++){
+//    for($j=1;$j<=$i;$j++){
+//        if($j==9){
+//            break;
+//        }else{
+//            echo $i."*".$j."=".$i*$j;
+//            echo "&nbsp;";
+//        }
+//    }
+//    echo "<br/>";
+//}
+
+for($i=1;$i<10;$i++){
+    for($j=1;$j<=$i;$j++){
+        if($j==9){
+            continue;
+        }else{
+            echo $i."*".$j."=".$i*$j;
+            echo "&nbsp;";
+        }
+    }
+    echo "<br/>";
+}
+
+//for($i=1;$i<10;$i++){
+//    for($j=1;$j<=$i;$j++){
+//        if($j==3){
+//            break;
+//        }else{
+//            echo $i."*".$j."=".$i*$j;
+//            echo "&nbsp;";
+//        }
+//    }
+//    echo "<br/>";
+//}
+
+for($i=1;$i<10;$i++){
+    for($j=1;$j<=$i;$j++){
+        if($j>=3){
+            continue;
+        }else{
+            echo $i."*".$j."=".$i*$j;
+            echo "&nbsp;";
+        }
+    }
+    echo "<br/>";
+}
+
+//for($i=1;$i<10;$i++){
+//    for($j=1;$j<=$i;$j++){
+//        if($i==4){
+//            break 2;
+//        }else if($j==3){
+//            break;
+//        }else{
+//            echo $i."*".$j."=".$i*$j;
+//            echo "&nbsp;";
+//        }
+//    }
+//    echo "<br/>";
+//}
+
+for($i=1;$i<10;$i++){
+    for($j=1;$j<=$i;$j++){
+        if($i>=4){
+            continue;
+        }else if($j>=3){
+            continue;
+        }else{
+            echo $i."*".$j."=".$i*$j;
+            echo "&nbsp;";
+        }
+    }
+    echo "<br/>";
+}
 ?>
